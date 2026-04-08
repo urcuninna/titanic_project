be82505e7af8c05a712c7e81dd96af928078f4b1
Erika Yesid <erika00yesid@gmail.com>
2026-04-07 21:58:35 -0500

    Limpieza completa: script, notebook y csv

--- scripts/data/clean_data.py
+++ scripts/data/clean_data.py
@@ -1,9 +1,18 @@
-# Limpieza de datos
-
-# Id
+# Limpieza de datos 
 
 import pandas as pd
 
-'''def convertir_id (df: pd.DataFrame, columna: str) -> pd.DataFrame:
-    df[columna] = df[columna].astype(str)
-    return df'''
+def limpiar_datos(df: pd.DataFrame) -> pd.DataFrame:
+    # copiar el DataFrame original para evitar modificarlo directamente
+    df_limpio = df.copy()
+
+    # eliminar columnas innecesarias
+    df_limpio = df_limpio.drop(columns=['Cabin', 'Ticket'])
+
+    # imputar valores faltantes en la columna 'Age' con la mediana
+    df_limpio['Age'] = df_limpio['Age'].fillna(df_limpio['Age'].median())
+
+    #imputar valores faltantes en la columna 'Embarked' con la moda
+    df_limpio['Embarked'] = df_limpio['Embarked'].fillna(df_limpio['Embarked'].mode()[0])
+
+    return df_limpio
